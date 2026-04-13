b6a91284ff2d34d0dafb76a37d0bd9a866823636
Carel Pretorius <cpretorius@avenirhealth.org>
2026-04-13 14:25:52 -0400

    run goals

--- goals/run_goals.py
+++ goals/run_goals.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from leapfrog_goals import get_goals_ss, run_goals
 from src.leapfrog_mapping.leapfrog_data_mapping import modvars_to_leapfrog
-
+from Tools.ImportPJNZ.Importer import GB_ImportProjectionFromFile
 """
 Note this script is only expected to be used temporarily until the
 proper data reading code is ready to go.
@@ -39,10 +39,26 @@ with open("zimbabwe_modvars.pickle", "wb") as handle:
     pickle.dump(numpy_modvars, handle, protocol=pickle.HIGHEST_PROTOCOL)
 """
 
-zim_pickle_path = os.path.expanduser("~/Downloads/zimbabwe_modvars.pickle")
+# zim_pickle_path = os.path.expanduser("~/Downloads/zimbabwe_modvars.pickle")
 
-with open(zim_pickle_path, "rb") as handle:
-    modvars = pickle.load(handle)
+# with open(zim_pickle_path, "rb") as handle:
+#     modvars = pickle.load(handle)
+
+
+
+modvars, param, epp, shiny90 = GB_ImportProjectionFromFile(r"C:\work\LeapFrog\V2\Files_PJNZ\Eswatini-2025-sqb.pjnz")
+
+for tag, value in modvars.items():
+    if isinstance(value, list):
+        try:
+            if len(value) > 0 and ((isinstance(value[0], dict)) or (isinstance(value[0], str)) or (isinstance(value[0], bool))):
+                value = np.array(value, order='C')
+            else:
+                value = np.array(value, order='C', dtype=np.dtype(np.float64))
+        except:
+            print(f'Failed to convert list to numpy array {tag}')
+
+    modvars[tag] = np.array(value)
 
 ss = get_goals_ss()
 lf_data = modvars_to_leapfrog(modvars, ss)
@@ -50,4 +66,4 @@ lf_data = modvars_to_leapfrog(modvars, ss)
 lf_data["ex_input"] = np.full((81, 2), 1)
 
 output = run_goals(lf_data)
-
+print(output)
