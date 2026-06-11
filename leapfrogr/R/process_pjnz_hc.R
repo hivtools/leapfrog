4e0523090bda5b0fef6bb33a1376ca0dbfd56180
Maggie <maggiek925@gmail.com>
2026-06-11 16:17:22 +0100

    robs requested changes

--- leapfrogr/R/process_pjnz_hc.R
+++ leapfrogr/R/process_pjnz_hc.R
@@ -413,10 +413,6 @@ process_pjnz_hc <- function(dat, pars, dim_vars, use_coarse_age_groups = FALSE, 
   pars$breastfeeding_duration_art <- pars$infant_feeding_options[, "art", ] / 100
   pars$breastfeeding_duration_no_art <- pars$infant_feeding_options[, "no art", ] / 100
 
-  ## Nosocomial infections by child age group (0-4, 5-9, 10-14); absent in older PJNZ files
-  if (!is.null(pars$nosocomial_infections_by_age)) {
-    pars$hc_nosocomial_infections_by_age <- pars$nosocomial_infections_by_age
-  }
   if (is.null(pars$hc_nosocomial_infections_by_age)) {
     pars$hc_nosocomial_infections_by_age <- matrix(0, nrow = 3, ncol = length(proj_years))
   }
--- scripts/create_test_data.R
+++ scripts/create_test_data.R
@@ -1,126 +1,41 @@
 #!/usr/bin/env Rscript
+# Creates HDF5 parameter files used by leapfrogr tests.
+# Run from the root of the leapfrogr package:
+#   Rscript scripts/create_test_data.R
 
-## This script creates test data required for the leapfrog tests.
-## We read some input data and prepare a set of demographic projection
-## and HIV parameters for both the adult and the child model.
-## We also run leapfrog and save out the result for use in reference tests
-setwd("leapfrogr")
-devtools::load_all()
+devtools::load_all(".")
 
-# nolint start
-library(dplyr)
+pkg_root <- here::here()
+test_data_dir <- file.path(pkg_root, "tests", "testthat", "testdata")
 
-## Create demographic and projection parameters for adults
-pjnz_adult <- file.path(here::here(), "inst", "pjnz", "bwa_aim-adult-art-no-special-elig_v6.13_2022-04-18.PJNZ")
+bwa_pjnz <- system.file(
+  "pjnz/bwa_aim-adult-art-no-special-elig_v6.13_2022-04-18.PJNZ",
+  package = "leapfrog"
+)
 
-parameters <- process_pjnz(pjnz_adult, extract_child_params = FALSE)
-save_parameters(parameters, testthat::test_path("testdata/adult_parms_full.h5"))
+bwa_pmtct_pjnz <- system.file(
+  "pjnz/bwa_aim-no-special-elig-numpmtct.PJNZ",
+  package = "leapfrog"
+)
 
-parameters_coarse <- process_pjnz(pjnz_adult, extract_child_params = FALSE, use_coarse_age_groups = TRUE)
-save_parameters(parameters_coarse, testthat::test_path("testdata/adult_parms_coarse.h5"))
+message("Creating adult_parms_full.h5 ...")
+pars_adult_full <- process_pjnz(bwa_pjnz)
+save_parameters(pars_adult_full, file.path(test_data_dir, "adult_parms_full.h5"))
 
-# We use France for testing Spectrum model variant as it has non-zero Non-AIDS excess mortality
-# inputs. Which we need for testing modelled non-AIDS excess mortality.
-# This was created by creating a new default projection in Spectrum
-pjnz_france <- file.path(here::here(), "inst", "pjnz", "france_default.PJNZ")
-france_parameters <- process_pjnz(pjnz_france, extract_child_params = TRUE)
-save_parameters(france_parameters, testthat::test_path("testdata/spectrum_params.h5"))
+message("Creating adult_parms_coarse.h5 ...")
+pars_adult_coarse <- process_pjnz(bwa_pjnz, use_coarse_age_groups = TRUE)
+save_parameters(pars_adult_coarse, file.path(test_data_dir, "adult_parms_coarse.h5"))
 
-#Create paeds parameters
-pjnz_child <- file.path(here::here(), "inst", "pjnz", "bwa_aim-no-special-elig-numpmtct.PJNZ")
+message("Creating child_parms_full.h5 ...")
+pars_child_full <- process_pjnz(bwa_pmtct_pjnz, extract_child_params = TRUE)
+save_parameters(pars_child_full, file.path(test_data_dir, "child_parms_full.h5"))
 
-parameters <- process_pjnz(pjnz_child, extract_child_params = TRUE,
-                           bypass_adult = TRUE)
-parameters_coarse <- process_pjnz(pjnz_child, extract_child_params = TRUE,
-                                  use_coarse_age_groups = TRUE, bypass_adult = TRUE)
+message("Creating child_parms_coarse.h5 ...")
+pars_child_coarse <- process_pjnz(
+  bwa_pmtct_pjnz,
+  use_coarse_age_groups = TRUE,
+  extract_child_params = TRUE
+)
+save_parameters(pars_child_coarse, file.path(test_data_dir, "child_parms_coarse.h5"))
 
-pop1 <- gsub(pattern = '.PJNZ', replacement = '_pop1.xlsx', x = pjnz_child)
-
-# Copied from EPPASM to remove cyclic dependency between leapfrog and eppasm
-read_pop1 <- function(pop1file, country, years = 2000:2021){
-
-  ## Read population out of Spectrum file
-  pop_list <- list()
-  for(yr in years){
-    pop1 <- as.data.frame(readxl::read_excel(pop1file, sheet=as.character(yr)))
-    pop1 <- pop1[-1,]
-    names(pop1)[1:3] <- c("sex", "cd4", "artdur")
-    pop1 <- stats::reshape(pop1, idvar=c("sex", "cd4", "artdur"), varying=c(0:79, "80+"), times=c(0:79, "80+"), timevar="age", v.names="pop", direction="long")
-    pop1$year <- as.integer(yr)
-    pop1$age <- as.integer(gsub("\\+", "", pop1$age))
-    pop1$cd4 <- as.integer(gsub("CD4 Count: ", "", pop1$cd4))
-    pop1$artdur <- as.integer(gsub("Duration: ", "", pop1$artdur))
-    pop_list[[as.character(yr)]] <- pop1
-  }
-  pop <- plyr::rbind.fill(pop_list)
-  pop$sex <- gsub("ales", "ale", pop$sex)
-  pop <- subset(pop, cd4 != 0 & artdur != 0 & pop > 0)
-
-  pop$country <- country
-
-  return(pop)
-}
-
-spectrum_output <- function(file, ages = 0:14, country = 'Botswana', years_in = 1970:2030){
-  ##pull out stratified population from the .xlsx file, This function doesn't take out the paediatric output, so going to just compare to the Spectrum software itself
-  df <- file
-
-  df <- read_pop1(df, country, years = years_in)
-    df_paed <- df %>% dplyr::filter(age < 5) %>%
-      dplyr::right_join(y = data.frame(cd4 = 1:8, cd4_cat = c('neg', 'gte30', '26-30', '21-25', '16-20', '11-15', '5-10', 'lte5'))) %>%
-      dplyr::right_join(y = data.frame(artdur = 2:8, transmission = c('perinatal', 'bf0-6', 'bf7-12', 'bf12+', 'ARTlte5mo', 'ART6to12mo', 'ARTgte12mo'))) %>%
-      dplyr::filter(cd4_cat != 'neg')
-
-    df_adol <- df %>% dplyr::filter(age > 4 & age < 15) %>%
-      dplyr::right_join(y = data.frame(cd4 = 3:8, cd4_cat = c('gte1000', '750-999', '500-749', '350-499', '200-349','lte200'))) %>%
-      dplyr::right_join(y = data.frame(artdur = 2:8, transmission = c('perinatal', 'bf0-6', 'bf7-12', 'bf12+', 'ARTlte5mo', 'ART6to12mo', 'ARTgte12mo'))) %>%
-      dplyr::filter(cd4_cat != 'neg')
-
-    df_paed <- rbind(df_paed, df_adol)
-
-    df <- df %>% dplyr::filter(age %in%  c(15:max(ages))) %>%
-      dplyr::right_join(y = data.frame(cd4 = 2:8, cd4_cat = c('gte500', '350-500', '250-349', '200-249', '100-199','50-99', 'lte50'))) %>%
-      dplyr::right_join(y = data.frame(artdur = 2:8, transmission = c('perinatal', 'bf0-6', 'bf7-12', 'bf12+', 'ARTlte5mo', 'ART6to12mo', 'ARTgte12mo'))) %>%
-      dplyr::filter(cd4_cat != 'neg')
-
-    df <- rbind(df, df_paed)
-  # setnames(df, 'cd4', 'cd4_cat')
-  df <- df %>% dplyr::select(sex, age, cd4_cat, year, pop, transmission)
-
-  df_on_treatment <- df[grepl('ART', df$transmission),] %>% dplyr::select(sex, age, cd4_cat, year, pop, transmission) %>% dplyr::group_by(sex, age, cd4_cat, transmission, year) %>% dplyr::mutate(pop = sum(pop)) %>% unique() %>% dplyr::filter(age %in% ages)
-  df_off_treatment <- df[!grepl('ART', df$transmission),]%>% dplyr::filter(age %in% ages)
-  df_total <- df %>% dplyr::select(sex, age, cd4_cat, year, pop) %>% dplyr::group_by(sex, age, cd4_cat, year) %>% dplyr::mutate(pop = sum(pop)) %>% unique()%>% dplyr::filter(age %in% ages)
-
-  return(list(on_treatment = df_on_treatment, off_treatment = df_off_treatment, total = df_total))
-
-}
-df <- spectrum_output(pop1, ages = 0:80, 'country', years_in = 1970:2030)
-x <- df$total
-
-dp <- pjnz:::read_dp_raw(pjnz_child)
-dat <- pjnz::read_dp(pjnz_child)
-
-aids_deathsnoart <- array(0, dim = c(15,2,61), dimnames = list(age = 0:14, sex = c('male', 'female'), years = 1970:2030))
-aids_deathsnoart[,'male',] <- dat$data$aids_deaths_no_art_single_age$data[as.character(0:14),"male",]
-aids_deathsnoart[,'female',] <- dat$data$aids_deaths_no_art_single_age$data[as.character(0:14),"female",]
-
-aids_deathsart <- array(0, dim = c(15,2,61), dimnames = list(age = 0:14, sex = c('male', 'female'), years = 1970:2030))
-aids_deathsart[,'male',] <- dat$data$aids_deaths_art_single_age$data[as.character(0:14),"male",]
-aids_deathsart[,'female',] <- dat$data$aids_deaths_art_single_age$data[as.character(0:14),"female",]
-
-spec_ctx_need <- dat$data$child_art_calc$data["both", "Children needing cotrim (0-14): ",]
-
-out <- list(dp = dp,
-            pjnz = pjnz_child,
-            pop1 = x,
-            ontrt = df$on_treatment,
-            offtrt = df$off_treatment,
-            deaths_noart = aids_deathsnoart,
-            deaths_art = aids_deathsart,
-            ctx_need = as.numeric(unlist(spec_ctx_need)))
-
-save_parameters(parameters, testthat::test_path("testdata/child_parms_full.h5"))
-save_parameters(parameters_coarse, testthat::test_path("testdata/child_parms_coarse.h5"))
-
-# need this for child model tests
-saveRDS(out, testthat::test_path("testdata/child_test_utils.rds"))
+message("Done. Test data files written to ", test_data_dir)
