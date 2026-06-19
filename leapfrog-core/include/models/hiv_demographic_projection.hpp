37918fd9d2d9169d5223dffbfbba056ac93d01f7
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2026-06-19 12:21:08 -0400

    safety first merge

--- leapfrog-core/include/models/hiv_demographic_projection.hpp
+++ leapfrog-core/include/models/hiv_demographic_projection.hpp
@@ -232,11 +232,11 @@ struct HivDemographicProjection<Config> {
         if (t > p_hc.hc_art_start) {
           for (int hu = 0; hu < hTS; ++hu) {
             if (t > opts.ts_art_start) {
-              n_vg.h_artpop_virgin(hu, hm, va, s) += i_hc.age15_artpop(hu, hm, s) * p_dp.survival_probability(a0, s, t);
+              n_vg.h_artpop_virgin(hu, hm, va0, s) += i_hc.age15_artpop(hu, hm, s) * p_dp.survival_probability(a0, s, t);
             } else {
               // If child ART has started, but not yet adult ART has started, put
               // children on ART into the untreated adult HIV population
-              n_vg.h_hivpop_virgin(hm, va, s) += i_hc.age15_artpop(hu, hm, s) * p_dp.survival_probability(a0, s, t);
+              n_vg.h_hivpop_virgin(hm, va0, s) += i_hc.age15_artpop(hu, hm, s) * p_dp.survival_probability(a0, s, t);
             }
           }
         }
