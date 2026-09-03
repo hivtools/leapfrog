2cf7ff4343ad706bea7eba94c40b59c9019b3fa2
elric <ewerst@avenirhealth.org>
2026-09-03 12:52:43 -0400

    exporting run model from state for dll used in spectrum desktop.

--- c_interface/c_interface.cpp
+++ c_interface/c_interface.cpp
@@ -115,3 +115,52 @@ DllExport HRESULT WINAPI run_aim_single_year(leapfrog::internal::CParams<double>
   return fit_model_single_year<leapfrog::Spectrum>(data, options, initial_state, simulation_start_year, out, error_handler);
 }
 
+template <typename ModelVariant>
+HRESULT fit_model_from_state(leapfrog::internal::CParams<double> &data,
+                             leapfrog::internal::COptions &options,
+                             leapfrog::internal::CState<double> &initial_state_data,
+                             int simulation_start_year,
+                             leapfrog::internal::CState<double> &out,
+                             CallbackFunction error_handler) {
+
+  using LF = leapfrog::Leapfrog<leapfrog::C, double, ModelVariant>;
+
+  try {
+    const leapfrog::Options<double> opts =  {
+      10,
+      options.ts_art_start,
+      options.proj_period,
+      options.proj_start_year,
+      options.proj_end_year
+    };
+    
+    std::vector<int> output_years((options.proj_end_year - options.proj_start_year) + 1);
+    std::iota(output_years.begin(), output_years.end(), options.proj_start_year);
+    const auto pars = LF::Cfg::get_pars(data, opts);
+    const auto initial_state = LF::Cfg::get_initial_state(initial_state_data);
+
+    auto state = LF::run_model_from_state(pars, opts, initial_state, simulation_start_year, output_years);
+    LF::Cfg::build_output(0, state, out);
+  } catch (const std::invalid_argument& e) {
+    error_handler(e.what());
+    return E_INVALIDARG;
+  } catch (...) {
+    error_handler("Caught unhandled exception");
+    return E_FAIL;
+  }
+
+  return S_OK;
+}
+
+DllExport HRESULT WINAPI run_model_from_state(leapfrog::internal::CParams<double> &data,
+                                              leapfrog::internal::COptions &options,
+                                              leapfrog::internal::CState<double> &initial_state,
+                                              int simulation_start_year,
+                                              leapfrog::internal::CState<double> &out,
+                                              CallbackFunction error_handler) {
+  #pragma EXPORT
+  return fit_model_from_state<leapfrog::Spectrum>(data, options, initial_state, simulation_start_year, out, error_handler);
+}
+
+
+
